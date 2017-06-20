02bfe59eba863c0d1a160331f9b00564c747778f
Will Taylor <willtaylor2352@gmail.com>
2017-06-19 21:29:35 -0400

    Added some notations

--- src/primeNumbers/PopulatePrimes.java
+++ src/primeNumbers/PopulatePrimes.java
@@ -2,11 +2,16 @@ package primeNumbers;
 
 import java.io.File;
 
+/**
+ * @author Will
+ * Populates a file with a list of prime numbers
+ */
 public class PopulatePrimes {
 
 	PopulatePrimes(){}
 	
 	/**
+	 * @author Will Taylor
 	 * Calculates prime numbers and puts them into a file
 	 * @param numberOfPrimesDesired The number of primes desired to be populated into file
 	 */
